5cd1894b12832abad860f7ba3de6ae81c9137dc5
Ben Frith <bpfrith@gmail.com>
2017-02-19 19:47:15 +0000

    some styling changes, errors fixed and making requests now, but need to get this close button to work

--- public/index.js
+++ public/index.js
@@ -1,6 +1,6 @@
 var app = function(){
-  var mapDiv = document.querySelector(#map);
-  var mapStyle = new MapStyle();
+  // var mapDiv = document.querySelector(#map);
+  // var mapStyle = new MapStyle();
   // var map = new MapWrapper(mapDiv, {lat: -34.427812, lng: 150.893061}, 12, mapStyle.getStyle());
   // var cityNow = {};
   // var builtCities = {};
@@ -23,28 +23,39 @@ var app = function(){
   }
 
   var setUkCoords = function (results, status) {
-      if (status === google.maps.GeocoderStatus.OK) {
-        // console.log(results);
-        output = {
-          name: results[0].formatted_address,
-          lat: results[0].geometry.location.lat(),
-          lng: results[0].geometry.location.lng(),
-          object: results[0]
-        }
-        // console.log(output);
-        var nameP = document.querySelector("#name");
-        var coordsP = document.querySelector("#coords");
-        // thisDiv.appendChild(nameP);
-        // thisDiv.appendChild(coordsP);
-        // test.appendChild(thisDiv);
-        nameP.innerText = output.name;
-        coordsP.innerText = output.lat + ", " + output.lng;
-        map.moveCenter({lat: output.lat, lng: output.lng});
-        getCurrentTime(output.lat, output.lng);
-      } else {
-        alert("Argh! Here there be monsters! : " + status);
+    if (status === google.maps.GeocoderStatus.OK) {
+      // console.log(results);
+      output = {
+        name: results[0].formatted_address,
+        lat: results[0].geometry.location.lat(),
+        lng: results[0].geometry.location.lng(),
+        object: results[0]
       }
+      // console.log(output);
+      var nameP = document.querySelector("#name");
+      var coordsP = document.querySelector("#coords");
+      // thisDiv.appendChild(nameP);
+      // thisDiv.appendChild(coordsP);
+      // test.appendChild(thisDiv);
+      nameP.innerText = output.name;
+      coordsP.innerText = output.lat + ", " + output.lng;
+      map.moveCenter({lat: output.lat, lng: output.lng});
+      getCurrentTime(output.lat, output.lng);
+    } else {
+      alert("Argh! Here there be monsters! : " + status);
     }
+  }
+
+  var requestUkCoords = function(city, country){
+    coordinator.geocode(city, country, setUkCoords);
+  }
+
+  var makeRequest = function (url, callback) {
+    var request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.onload = callback;
+    request.send();
+  }
 
 }
 
