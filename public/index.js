61129baae8c3e5f15239665f2845247b8cb53e47
Ben Frith <bpfrith@gmail.com>
2017-02-19 16:42:17 +0000

    more added to index.js and express is now working.

--- public/index.js
+++ public/index.js
@@ -1,5 +1,27 @@
 var app = function(){
+  var mapDiv = document.querySelector(#map);
+  var mapStyle = new MapStyle();
+  // var map = new MapWrapper(mapDiv, {lat: -34.427812, lng: 150.893061}, 12, mapStyle.getStyle());
+  // var cityNow = {};
+  // var builtCities = {};
+  // var ukCities = new UkCities();
+  // var coordinator = new Coordinator();
 
+  var ukCitiesCoords = {};
+
+  var postCurrentTime = function(timeObject){
+    var localTime = document.querySelector("#time");
+    localTime.innerText = timeObject.formatted;
+  }
+
+  var getCurrentTime = function(lat, lng){
+    var url = "https://api.timezonedb.com/v2/get-time-zone?key=AFQAZRVOAIDL&format=json&by=position&lat=" + lat + "&lng=" + lng;
+    makeRequest(url, function(){
+      console.log(JSON.parse(this.responseText));
+      postCurrentTime(JSON.parse(this.responseText));
+    })
+  }
+  
 }
 
 window.onload = app;
